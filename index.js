e0c0669cc265ea7d7ca24f5e0074aa7ca7498989
gomesht <gomesht@hotmail.com>
2022-11-25 21:37:59 -0300

    função para mostrar e escoder certificados

---
+++ index.js
@@ -0,0 +1,27 @@
+var certificados = document.getElementById('certificados')
+var certificadoHTML = document.getElementById('certificadoHtml')
+var certificadoCSS = document.getElementById('certificadoCss')
+var certificadoJSBasico = document.getElementById('certificadoJsBasico')
+
+
+function clickEvent(props) {
+    switch (props.className) {
+        case "mostrar":
+            props.classList.remove('mostrar');
+            props.classList.add('esconder');
+
+
+            break
+        case "esconder":
+            props.classList.remove('esconder');
+            props.classList.add('mostrar');
+
+            break
+        default:
+            console.error("Erro de classe no certificado");
+            break
+
+    }
+
+
+}
